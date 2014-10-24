00e23de764c06c231d78039518277d1c865852a0
Sapein <Chandler182@gmail.com>
2014-10-24 16:26:19 -0500

    Changed some comments, added license
    Alright so I changed some comments and stuff. Since this is using Pircbot
    I kinda have to license the bot under GNU GPL v2.0 as such, I have now
    done so. Also this should be the final version of Zatch, unless there is a
    bug or something. I might be working on another bot, but not atm.

--- ZatchBot.java
+++ ZatchBot.java
@@ -1,3 +1,21 @@
+/*A Simple IRC bot that uses PIRC
+    Copyright (C) 2014  
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
 //Created by Sapein.
 //Zatchbot v 2.0 BETA
 import java.util.regex.Matcher;
@@ -389,9 +407,9 @@ public class ZatchBot extends PircBot
 	}
 	}
 	protected void onDisconnect(){ 
-		while (!isConnected()) { //if it looses connection to the server
+		while (!isConnected()) { //To test when connection is lost
 		    try {
-		        reconnect(); //it will try to reconnect
+		        reconnect(); //To have the bot reconnect
 		    }
 		    catch (Exception e) {
 		    }
