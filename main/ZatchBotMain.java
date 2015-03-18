46f3facb18186e9ccfe57265994558cb9630d583
Chandler <chandler182@gmail.com>
2015-03-18 16:31:37 -0500

    Began work on rewrite
    CHANGED: Reverted ZatchBot.java a little

--- main/ZatchBotMain.java
+++ main/ZatchBotMain.java
@@ -1,21 +1,22 @@
 package main;
+
 /*A Simple IRC bot that uses PIRC
-    Copyright (C) 2014  
+ Copyright (C) 2014  
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License along
-    with this program; if not, write to the Free Software Foundation, Inc.,
-    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-*/
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 //Created by Sapein
 //Zatchbot v 1.0
 
@@ -23,21 +24,39 @@ import java.io.*;
 
 import org.jibble.pircbot.*;
 
-public class ZatchBotMain{
+public class ZatchBotMain {
 
 	public static void main(String[] arg) throws Exception {
 		ZatchBotConfigStartup ConfigStartup = new ZatchBotConfigStartup();
-		
+
 		ConfigStartup.generateConfig();
 		ConfigStartup.loadConfigStage1();
 		String Server = ConfigStartup.getServer();
 		String[] Channels = ConfigStartup.getChannels();
-		
-		ZatchBot bot = new ZatchBot();      // this starts your bot
-		bot.setVerbose(true);             // enable debugging, useful during programming
-		bot.connect(Server); //Connect to the Server(The Server Variable is called here)
-		for(int ChannelAmount = 0; ChannelAmount < Channels.length; ++ChannelAmount) { //This makes sure that all Channels listed are joined as we stored it in a variable earlier.
-			bot.joinChannel(Channels[ChannelAmount]); //Actually Joins the channels. 
-  		}
+
+		ZatchBot bot = new ZatchBot(); // this starts your bot
+		bot.setVerbose(true); // enable debugging, useful during programming
+		bot.connect(Server); // Connect to the Server(The Server Variable is
+								// called here)
+		for (int ChannelAmount = 0; ChannelAmount < Channels.length; ++ChannelAmount) { // This
+																						// makes
+																						// sure
+																						// that
+																						// all
+																						// Channels
+																						// listed
+																						// are
+																						// joined
+																						// as
+																						// we
+																						// stored
+																						// it
+																						// in
+																						// a
+																						// variable
+																						// earlier.
+			bot.joinChannel(Channels[ChannelAmount]); // Actually Joins the
+														// channels.
+		}
 	}
 }
